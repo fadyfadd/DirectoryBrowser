cd7a07120627ea4a332f8ddd3a521783c8cc2e80
fadyfadd <fady_faddoul@hotmail.com>
2021-03-25 19:25:23 +0200

    Add project files.

---
+++ DirectoryBrowser/ImageConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Linq;
+
+namespace DirectoryBrowser
+{
+    public class ImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            String path = value.ToString();
+
+            DirectoryInfo info = new DirectoryInfo(path);
+
+            var isDrive = Directory.GetLogicalDrives().Where(p => p.ToUpper().Equals(path.ToUpper())).FirstOrDefault();
+
+            if (isDrive != null)
+                return new BitmapImage(new Uri("pack://application:,,,/Images/Drive.png"));
+
+
+            if ((info.Attributes & FileAttributes.Directory) == FileAttributes.Directory)
+            {
+                return new BitmapImage(new Uri("pack://application:,,,/Images/Folder.png"));
+            }
+
+            return new BitmapImage(new Uri("pack://application:,,,/Images/File.png"));
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
---
+++ DirectoryBrowser/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+ 
+
+namespace DirectoryBrowser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            var drives = Directory.GetLogicalDrives();         
+           
+
+            foreach (var drive in drives)
+            {
+                var currentItem = new TreeViewItem();
+                currentItem.Items.Add(null);                
+                currentItem.Header = drive.ToString();
+                currentItem.Tag = drive.ToString();
+                treeView.Items.Add(currentItem);
+
+                currentItem.Expanded += CurrentItem_Expanded;
+              
+            }
+        }
+
+        private void CurrentItem_Expanded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+               
+                var currentItem = (TreeViewItem)sender;
+
+                if (currentItem.Items.Count ==0 || currentItem.Items[0] != null)
+                    return;
+
+              
+                currentItem.Items.RemoveAt(0);
+
+                var tag = currentItem.Tag;
+                var entries = Directory.GetFileSystemEntries(tag.ToString());
+
+                foreach (String entry in entries)
+                {
+                    DirectoryInfo info = new DirectoryInfo(entry);
+
+                   
+                    
+
+                    var fileName = System.IO.Path.GetFileName(entry);
+                    var itemToAdd = new TreeViewItem();
+                    if ((info.Attributes & FileAttributes.Directory) == FileAttributes.Directory)
+                        itemToAdd.Items.Add(null);
+                    itemToAdd.Header = fileName;
+                    itemToAdd.Tag = entry.ToString();
+                    itemToAdd.Expanded += CurrentItem_Expanded;
+                    currentItem.Items.Add(itemToAdd);
+                   
+                }
+                
+            }
+            catch
+            {
+
+            }    
+
+        }
+    }
+}
